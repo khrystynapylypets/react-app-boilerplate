6464813eaf4d2faf856c57d4cf3ee749186a9112
Khrystyna Pylypets <khrystyna.pylypets@gmail.com>
2026-01-05 14:31:26 +0200

    Running webpack on different port if 3000 is busy

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,49 @@
 import { merge } from 'webpack-merge';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { detect } from 'detect-port';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { commonConfig } from './webpack.common.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const defaultPort = 3000;
 
-export default merge(commonConfig, {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    port: 3000, // TODO: generate port
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ]
-})
+const generateDevConfig = async () => {
+  const getAvailablePort = async (startingPort) => {
+    try {
+      const availablePort = await detect(startingPort);
+      console.log(`Port ${availablePort} is available`);
+      return availablePort;
+    } catch {
+      const nextPort = startingPort + 1;
+      console.log(`Port ${startingPort} is occupied, trying ${nextPort}`);
+      return getAvailablePort(nextPort);
+    }
+  };
+
+  return {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      port: await getAvailablePort(defaultPort),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    ],
+  };
+};
+
+export default async () => {
+  const devConfig = await generateDevConfig();
+  return merge(commonConfig, devConfig);
+};
