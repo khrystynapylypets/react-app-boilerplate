0e3ef877bab5e902a9f911c93d8224bd91f16503
Khrystyna Pylypets <khrystyna.pylypets@gmail.com>
2026-01-07 18:14:06 +0200

    feature(TS): Fix TS config, make it work in IDE

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client';
 import App from '@components/App';
 
 const rootNode = document.getElementById('root');
-const root = createRoot(rootNode);
 
-root.render(<App />);
+if (rootNode) {
+  const root = createRoot(rootNode);
+
+  root.render(<App />);
+}
